d913639e2d7c37eb4931b1642cb04101e86b5010
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2025-05-01 18:06:41 +0300

    Added HomeScreen + LoginScreen + realized navigation

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import HomeScreen from '../screens/Home/HomeScreen'
+import LoginScreen from '../screens/Auth/LoginScreen'
+
+const Stack = createStackNavigator()
+
+export default function AppNavigator () {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
